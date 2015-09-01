cc2f4f70d679fe64fc199466e89b70a1ab28f8a7
Charles Kollar <chuck@kollar.com>
2015-09-01 11:36:10 -0400

    Removed static directory

--- application.py
+++ application.py
@@ -29,8 +29,7 @@ Using application.py as the filename and providing a callable application object
 (the Flask object, in this case) allows AWS Elastic Beanstalk to easily find your
 application's code.
 '''
-#application = Flask(__name__, template_folder=".")
-application = Flask(__name__)
+application = Flask(__name__, template_folder=".")
 
 
 '''
@@ -87,7 +86,6 @@ def get_day_time_temp_dewpt(lat, lon):
     return ([], [], [])
 
 @application.route('/', methods=['GET', 'POST'])
-@application.route('/index', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         # This will put up a map that is geolocated to the user...
@@ -107,4 +105,4 @@ if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     application.debug = True
-    application.run(host='0.0.0.0.')
+    application.run()
